d9278090fe6de2d775fdbfb40c74cd6712ed7411
tmenezes <thiagomenezes2k7@gmail.com>
2021-04-01 01:00:12 +0200

    Added missing assets 2

---
+++ Assets/Scripts/GamePlay/FX/VisualEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Space_Shooter_Template_FREE.Scripts
+{
+    /// <summary>
+    /// This script attaches to ‘VisualEffect’ objects. It destroys or deactivates them after the defined time.
+    /// </summary>
+    public class VisualEffect : MonoBehaviour {
+
+        [Tooltip("the time after object will be destroyed")]
+        public float destructionTime;
+
+        private void OnEnable()
+        {
+            StartCoroutine(Destruction()); //launching the timer of destruction
+        }
+
+        IEnumerator Destruction() //wait for the estimated time, and destroying or deactivating the object
+        {
+            yield return new WaitForSeconds(destructionTime); 
+            Destroy(gameObject);
+        }
+    }
+}
